553cf147522a1e67c4efd532254a92749b3757aa
andreiPereira <andrei.s.pereira.ibi@gmail.com>
2022-03-09 22:33:58 -0300

    Novo metodo de usar variavel ${}

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+/*
 var idade = 23;
 var nome = "Andrei";
-
+console.log("Salvando em variáveis:");
 console.log(nome);
 console.log(idade);
-console.log(idade+2);
+
+console.log("Convertendo texto para numero:");
+console.log(parseInt("4") + parseInt("4"));
+console.log("4" + "4"); 
+*/
+
+
+console.log("Chamar variáveis:");
+const nome = "Andrei";
+const sobrenome = "Pereira";
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+
