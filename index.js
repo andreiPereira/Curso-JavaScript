44236a8ff241c8492b2ff75446000c8596ffb727
andreiPereira <andrei.s.pereira.ibi@gmail.com>
2022-03-13 10:46:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-/*
-var idade = 23;
-var nome = "Andrei";
-console.log("AULA 1 = Salvando em variáveis:");
-console.log(nome);
-console.log(idade);
 
-console.log("AULA 2 = Convertendo texto para numero:");
-console.log(parseInt("4") + parseInt("4"));
-console.log("4" + "4"); 
+const salario = 3300.0;
 
-console.log("AULA 3 = Chamar variáveis:");
-const nome = "Andrei";
-const sobrenome = "Pereira";
-console.log(`Meu nome é ${nome} ${sobrenome}`);
-
-console.log("AULA 4 = Trabalhando com Array");
-let kaisa = `Kaisa`;
-let vayne = `Vayne`;
-let jinx = `Jinx`;
-let adcLol = new Array(
-    `Kaisa`,
-    `Vayne`,
-    `Jinx`
-);
-console.log(adcLol);
-
-//push irá 'empurrar' um item para dentro da lista 
-adcLol.push(`Jhin`);
-console.log(adcLol);
-
-//splice para deletar por posição (posição , quantidade)
-adcLol.splice(2 , 1);
-console.log(adcLol);
-
-//Escolhendo posições específicas 
-console.log(adcLol[1]);
-
-*/
+if(salario < 2600.0) {
+    console.log("A sua aliquota é de 15%");
+    console.log("Você pode deduzir até R$ 350");
+}        
+else if(salario < 3750.0) {
+    console.log("A sua aliquota é de 22,5%");
+    console.log("Você pode deduzir até R$ 636");
+}
