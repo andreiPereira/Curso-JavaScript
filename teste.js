d8163a9c179cae557ec6dcb2bc0981a92a5d0ce0
andreiPereira <andrei.s.pereira.ibi@gmail.com>
2022-03-15 06:59:38 -0300

    conculusão curso 1 formação node

---
+++ teste.js
@@ -0,0 +1,16 @@
+
+class cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new cliente();
+
+cliente1.nome = "Andrei";
+cliente1.cpf = 04330968072;
+cliente1.agencia = 0001;
+cliente1.saldo = 15;
+
+console.log(cliente1);
