60ce26caf4caeecdf177e8925089d85dd070447f
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-19 14:25:18 -0300

    Conclusão Heranças

--- Conta.js
+++ Conta.js
@@ -1,43 +1,46 @@
-export class Conta{
-    constructor(tipo, saldoInicial, cliente, agencia) {
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
-        this._tipo = tipo;
+       
+    }
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
     }
 
     sacar(valor) {
+        let taxa = 1
+        return this._sacar(valor, taxa);
+    }
 
-        let taxa = 1;
-        if(this._tipo == "corrente"){
-            taxa = 1.1;
-        }
-        if(this._tipo == "salario"){
-            taxa = 1.05;
-        }
-        if(this._tipo == "empresarial"){
-            taxa = 1.15;
-        }
+    _sacar(valor, taxa){
         const valorSacado = taxa * valor;
         if (this._saldo >= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
         }
+
+        return 0;
     }
 
     depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
         this._saldo += valor;
     }
 
     tranferir(valor, conta) {
-        if(this._tipo == "salario"){
-            return;
-        }
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-
     }
 }
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,12 +1,15 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente extends Conta {
+export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
-
-    constructor(agencia, cliente) {
-  
+    constructor(cliente, agencia) {
+        super(0,cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
 
-
+    //sobreescrevendo o comportamento de sacar
+    sacar(valor) {
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
+    }
 }
